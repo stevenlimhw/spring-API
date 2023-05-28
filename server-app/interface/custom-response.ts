e694066c2669c7d901f8fc59b0ad266e70368a46
Steven Lim <limhwsteven@gmail.com>
2023-05-28 14:59:14 +0800

    Add classes and service

--- server-app/interface/custom-response.ts
+++ server-app/interface/custom-response.ts
@@ -1,3 +1,14 @@
-export interface CustomResponse {
+import {Server} from "./server";
 
+export interface CustomResponse {
+  timeStamp: Date;
+  statusCode: number;
+  status: string;
+  reason: string;
+  message: string;
+  developerMessage: string;
+  data: {
+    servers?: Server[],
+    server?: Server
+  };
 }
